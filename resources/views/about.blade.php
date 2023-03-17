05ee4c01750b6f5c032e4ba89bc1bf0f8d4af52e
eko020301 <eko020301@gmail.com>
2023-03-17 22:37:58 +0700

    v.1.1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+<section class="mt-5 bg-danger-subtle">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col mt-5">
+                <h1 class="border-bottom border-dark">ABOUT</h1>
+            </div>
+        </div>
+        <div class="row mt-5 pb-5 justify-content-center text-center">
+            <div class="col-md-3 mb-3">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, perferendis molestiae dicta explicabo temporibus deserunt cum cupiditate perspiciatis nihil suscipit eum! Aspernatur ducimus eos atque?</p>
+            </div>
+            <div class="col-md-3">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita harum eveniet culpa dicta pariatur vel adipisci reprehenderit porro eligendi, tempore provident fuga similique cum consequatur fugit totam, itaque dignissimos necessitatibus?</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-5" style="background-color: #ffc107">
+    <div class="container">
+        <div class="row pb-5 justify-content-center text-center">
+            <div class="col-md-3 mb-3">
+                <h3 class="mb-4">ALAMAT</h3>
+                <p>Jl. Bunga Truly No.23</p>
+            </div>
+            <div class="col-md-3">
+                <h3 class="mb-4">SOSMED</h3>
+                <a href="https://www.instagram.com/otnawrup_/" class="text-dark text-decoration-none fw-bold">Instagram</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    
+</footer>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+<div class="container mt-5">
+    <div class="row">
+        <h1 class="mt-5 text-center">Ini Halaman Home</h1>
+    </div>
+</div>
